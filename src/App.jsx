819eea5284ef588c577f8282ed095f9c47a2bdc0
Polyane Tuag <pollyportugues@gmail.com>
2025-06-03 20:30:12 -0300

    feat: Add CardList component to App for displaying Pokémon cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { CardList } from "./components/CardList";
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <CardList />
     </div>
   );
 }
