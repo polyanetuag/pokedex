1729315eff7cf188376b0921da1f4cf96ebd914b
Polyane Tuag <pollyportugues@gmail.com>
2025-06-05 18:57:40 -0300

    fix: Wrap image in a div for better structure in CardItem component

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -5,10 +5,13 @@ export function CardItem() {
     <div className="card-list-details">
       <span className="card-list-details-code">#001</span>
       <div className="card-list-details-img">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt=""
-        />
+        <div>
+          <img
+            width="100%"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            alt=""
+          />
+        </div>
       </div>
       <span className="card-list-details-name">Bulbasaur</span>
       <div className="card-list-details-background"></div>
