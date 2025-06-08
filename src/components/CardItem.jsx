3df425c9a5ebbc315448e0e31816bb7f14f22c8d
Polyane Tuag <pollyportugues@gmail.com>
2025-06-08 12:51:56 -0300

    feat: Implement CardItem component to fetch and display Pokémon details

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,20 +1,43 @@
+import { Link } from "react-router";
 import "./CardItem.css";
+import { useEffect, useState } from "react";
 
-export function CardItem() {
+export function CardItem({ name, url }) {
+  const [pokemonCard, setPokemonCard] = useState([]);
+
+  useEffect(() => {
+    const pokemonDetailsData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setPokemonCard(json);
+    };
+
+    if (url) {
+      pokemonDetailsData();
+    }
+  }, [url]);
+
+  if (!pokemonCard) {
+    return null;
+  }
+
   return (
-    <div className="card-list-details">
-      <span className="card-list-details-code">#001</span>
-      <div className="card-list-details-img">
-        <div>
-          <img
-            width="100%"
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt=""
-          />
+    <Link to={`/${pokemonCard.id}`} className="link">
+      <div className="card-list-details">
+        <span className="card-list-details-code">{`#${pokemonCard.id}`}</span>
+        <div className="card-list-details-img">
+          <div>
+            <img
+              width="100%"
+              src={pokemonCard.sprites?.other["official-artwork"].front_default}
+              alt=""
+            />
+          </div>
         </div>
+        <span className="card-list-details-name">{name}</span>
+        <div className="card-list-details-background"></div>
       </div>
-      <span className="card-list-details-name">Bulbasaur</span>
-      <div className="card-list-details-background"></div>
-    </div>
+    </Link>
   );
 }
