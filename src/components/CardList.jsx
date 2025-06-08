d37fda12c8b126ece23cf3d639be454fbadb4b6d
Polyane Tuag <pollyportugues@gmail.com>
2025-06-08 12:52:37 -0300

    feat: Fetch and display Pokémon data in CardList component

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react";
 import { CardItem } from "./CardItem";
 import "./CardList.css";
+import { ItemDetails } from "./ItemDetails";
 
 export function CardList() {
+  const [openDetails, setOpenDetails] = useState(false);
+  const [cardList, setCardList] = useState([]);
+
+  const pokemonData = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const json = await response.json();
+    const result = json.results;
+
+    setCardList(result);
+  };
+
+  const handleItemClick = () => {
+    console.log("Card item clicado:");
+    setOpenDetails(!openDetails);
+  };
+
+  useEffect(() => {
+    pokemonData();
+  }, []);
+
   return (
     <section className="card-list-section">
-      <div className="card-list">
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
+      <div className="card-list" onClick={handleItemClick}>
+        {cardList?.map(({ name, url }) => (
+          <CardItem key={name} name={name} url={url} />
+        ))}
       </div>
+
+      {openDetails && <ItemDetails />}
     </section>
   );
 }
