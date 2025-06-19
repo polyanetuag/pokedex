7aa0aee9bd1eb4ba09259a8880497cf0521b0540
Polyane Tuag <pollyportugues@gmail.com>
2025-06-19 14:26:47 -0300

    refactor: Simplify Header component by removing unused state and search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,6 @@
-import { useState } from "react";
-import { HashIcon } from "../icons/HashTag";
-import { SearchIcon } from "../icons/Search";
 import "./Header.css";
 
 export function Header() {
-  const [openMenu, setOpenMenu] = useState(false);
-
-  const handleClick = () => {
-    if (openMenu) {
-      setOpenMenu(false);
-    } else {
-      setOpenMenu(true);
-    }
-  };
-
   return (
     <header className="header-container">
       <div className="header-content">
@@ -23,46 +10,6 @@ export function Header() {
           alt="Logo pokeball"
         />
         <h2 className="header-title">Pokédex</h2>
-      </div>
-
-      <div className="header-search">
-        <input
-          className="header-search-input"
-          type="text"
-          placeholder="Search"
-        />
-
-        <div className="dropdown">
-          <button className="drowpdown-trigger" onClick={handleClick}>
-            <HashIcon />
-          </button>
-          {openMenu && (
-            <div className="dropdown-content">
-              <p>Sort by:</p>
-              <div className="dropdown-items-content">
-                <div className="dropdown-items">
-                  <input
-                    className="dropdown-items-input"
-                    type="radio"
-                    checked
-                  />
-                  <label
-                    className="dropdown-items-label"
-                    htmlFor="input-number"
-                  >
-                    Number
-                  </label>
-                </div>
-                <div className="dropdown-items">
-                  <input className="dropdown-items-input" type="radio" />
-                  <label className="dropdown-items-label" htmlFor="input-name">
-                    Name
-                  </label>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
       </div>
     </header>
   );
